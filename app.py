ae7d4f986b6b23e53131889a4bc7212cf75bcee3
Kavya Shah <kavya.25bcs10125@sst.scaler.com>
2026-04-08 17:32:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import sys
+import os
+# This line tells the folder to look outside itself to find your env.py file!
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from fastapi import FastAPI
+from env import EmailEnv, Action
+
+app = FastAPI()
+office = EmailEnv(num_emails=10)
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.post("/reset")
+def reset():
+    obs = office.reset()
+    return obs.model_dump()
+
+@app.get("/state")
+def state():
+    obs = office.state()
+    return obs.model_dump()
+
+@app.post("/step")
+def step(action: Action):
+    obs, reward, done, info = office.step(action)
+    return {
+        "observation": obs.model_dump(),
+        "reward": reward.value,
+        "done": done,
+        "info": info
+    }
+
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
